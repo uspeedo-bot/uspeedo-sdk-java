6524e14d4e195014a866b8079687920839b33f9c
xuan <xuan.zhao@ucloud.cn>
2024-06-17 17:19:21 +0800

    sdk: rolling update for 1.4.0-release

---
+++ uspeedo-sdk-java-vms/src/main/java/com/uspeedo/vms/client/VMSClientInterface.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2021 USpeedo Technology Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.uspeedo.vms.client;
+
+import com.uspeedo.common.client.Client;
+import com.uspeedo.common.exception.USpeedoException;
+        
+        
+        
+import com.uspeedo.vms.models.SendMessageRequest;
+import com.uspeedo.vms.models.SendMessageResponse;
+
+/**
+ * This client is used to call actions of **VMS** service
+ */
+public interface VMSClientInterface extends Client {
+        
+        
+        
+        
+        
+        
+    /**
+     * SendVMSMessage - 
+     *
+     * @param request Request object
+     * @throws USpeedoException Exception
+     */
+    public SendMessageResponse sendVMSMessage(SendMessageRequest request) throws USpeedoException;
+}
